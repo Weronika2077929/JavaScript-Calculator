3fd28b7f05dbe71af549cf950dae81544638fed4
JeszuaHaNocri <lachtaraweronika@gmail.com>
2015-10-26 13:08:00 +0000

    Changed the bar chart into multiple bar chart + improved css of different designs

--- logic.js
+++ logic.js
@@ -74,11 +74,14 @@ function printMousePos(e) {
             d3.select('svg').remove();
             d3.select('svg').remove();
             fittsLaw();
-            var errorList = [{name: "design 1", counter: errorDict.design1 , color:"red"},
-                {name: "design 2", counter: errorDict.design2, color: "blue"},
-                {name: "design 3", counter: errorDict.design3, color: "purple"} ];
+            //var errorList = [{name: "design 1", counter: errorDict.design1 , color:"red"},
+            //    {name: "design 2", counter: errorDict.design2, color: "blue"},
+            //    {name: "design 3", counter: errorDict.design3, color: "purple"} ];
             //console.log(errorList[0].counter + " " + errorList[1].counter + " " + errorList[2].counter);
-            barChart(errorList);
+            //barChart(errorList);
+            var errorList = [errorDict.design1, errorDict.design2, errorDict.design3];
+            var clickList = [clickDict.design1, clickDict.design2, clickDict.design3];
+            multiBar(errorList, clickList);
         }
         else if(button.value == '<--'){
             errorCount();
@@ -301,6 +304,7 @@ function barChart(data) {
     var yAxis = d3.svg.axis()
         .scale(y)
         .orient("left")
+        .innerTickSize(-width)
         .tickFormat(d3.format("d"));
 
     var svg = d3.select("body").append("svg")
@@ -339,4 +343,133 @@ function barChart(data) {
             .attr("height", function(d) { return height - y(d.counter); })
             .style("fill" , function(d) {return d.color});
 
+}
+
+
+
+
+/// multiple bar
+
+function multiBar(errorList, clickList){
+console.log(clickList);
+    var data = {
+    labels: [
+        'design 1', 'design 2', 'design 3'
+    ],
+    series: [
+        {
+            label: 'errors',
+            values: errorList
+        },
+        {
+            label: 'all clicks',
+            values: clickList
+        }]
+};
+
+    var chartWidth       = 600,
+        barHeight        = 50,
+        groupHeight      = barHeight * data.series.length,
+        gapBetweenGroups = 10,
+        spaceForLabels   = 150,
+        spaceForLegend   = 150;
+
+// Zip the series data together (first values, second values, etc.)
+    var zippedData = [];
+    for (var i=0; i<data.labels.length; i++) {
+        for (var j=0; j<data.series.length; j++) {
+            zippedData.push(data.series[j].values[i]);
+        }
+    }
+
+// Color scale
+    var color = d3.scale.category20();
+    var chartHeight = barHeight * zippedData.length + gapBetweenGroups * data.labels.length;
+
+    var x = d3.scale.linear()
+        .domain([0, d3.max(zippedData)])
+        .range([0, chartWidth]);
+
+    var y = d3.scale.linear()
+        .range([chartHeight + gapBetweenGroups, 0]);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .tickFormat('')
+        .tickSize(0)
+        .orient("left");
+
+// Specify the chart area and dimensions
+    var chart = d3.select("#graph3")
+        .append('svg:svg')
+        .attr("width", spaceForLabels + chartWidth + spaceForLegend)
+        .attr("height", chartHeight);
+
+// Create bars
+    var bar = chart.selectAll("g")
+        .data(zippedData)
+        .enter().append("g")
+        .attr("transform", function(d, i) {
+            return "translate(" + spaceForLabels + "," + (i * barHeight + gapBetweenGroups * (0.5 + Math.floor(i/data.series.length))) + ")";
+        });
+
+// Create rectangles of the correct width
+    bar.append("rect")
+        .attr("fill", function(d,i) { return color(i % data.series.length); })
+        .attr("class", "bar")
+        .attr("width", x)
+        .attr("height", barHeight - 1);
+
+// Add text label in bar
+    bar.append("text")
+        .attr("x", function(d) { return x(d); })
+        .attr("y", barHeight / 2)
+        .attr("fill", "red")
+        .attr("dy", ".35em")
+        .text(function(d) { return d; });
+
+// Draw labels
+    bar.append("text")
+        .attr("class", "label")
+        .attr("x", function(d) { return - 80; })
+        .attr("y", groupHeight / 2)
+        .attr("dy", ".35em")
+        .text(function(d,i) {
+            if (i % data.series.length === 0)
+                return data.labels[Math.floor(i/data.series.length)];
+            else
+                return ""});
+
+    chart.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + spaceForLabels + ", " + -gapBetweenGroups/2 + ")")
+        .call(yAxis);
+
+// Draw legend
+    var legendRectSize = 18,
+        legendSpacing  = 4;
+
+    var legend = chart.selectAll('.legend')
+        .data(data.series)
+        .enter()
+        .append('g')
+        .attr('transform', function (d, i) {
+            var height = legendRectSize + legendSpacing;
+            var offset = -gapBetweenGroups/2;
+            var horz = spaceForLabels + chartWidth + 40 - legendRectSize;
+            var vert = i * height - offset;
+            return 'translate(' + horz + ',' + vert + ')';
+        });
+
+    legend.append('rect')
+        .attr('width', legendRectSize)
+        .attr('height', legendRectSize)
+        .style('fill', function (d, i) { return color(i); })
+        .style('stroke', function (d, i) { return color(i); });
+
+    legend.append('text')
+        .attr('class', 'legend')
+        .attr('x', legendRectSize + legendSpacing)
+        .attr('y', legendRectSize - legendSpacing)
+        .text(function (d) { return d.label; });
 }
